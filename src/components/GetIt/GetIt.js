429a7472d7b77d434babf1ba5521c9a550159a99
timoxin.artur@gmail.com <timoxin.artur@gmail.com>
2019-02-10 18:57:26 +0200

    initial commit, start design, architecture basic react

---
+++ src/components/GetIt/GetIt.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function GetIt() {
+  return (
+    <div className="wrapper-get-it">
+      <div className="wrapper-text">
+        <h3>Monstroid² is the perfect start of your future website</h3>
+        <p>We took into consideration all of the latest innovative user-experience researches and best practices.</p>
+      </div>
+      <button className="get-it">GET IT NOW</button>
+    </div>
+  );
+}
